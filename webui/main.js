88fd511b0c2a002971f294945309e4c0e98ed7e7
Locke21 <fabian.hilz@t-online.de>
2016-10-14 08:57:32 +0200

    playing with transitions

--- webui/main.js
+++ webui/main.js
@@ -12,6 +12,7 @@ noobcorner.config(function($routeProvider, $locationProvider){
 });
 
 noobcorner.controller('MainCtrl', function($scope){
+  $scope.shrink = false;
 
   $scope.toggleNavBar = function(){
     $('.navItemsRight').removeClass('shrink');
@@ -20,22 +21,29 @@ noobcorner.controller('MainCtrl', function($scope){
   }
 
   $(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('.myNavbar').addClass('shrink');
-    $('.myNavbar-nav').addClass('shrink');
-    $('.myBrand').addClass('shrink');
+     $scope.$apply(function() {
+      if ($(document).scrollTop() > 50) {
 
-    $('.navItemsRight').addClass('shrink');
-    $('.navItemsLeft').addClass('shrink');
-    $('.navLogo').addClass('shrink');
-  } else {
-    $('.myNavbar').removeClass('shrink');
-    $('.myNavbar-nav').removeClass('shrink');
-    $('.myBrand').removeClass('shrink');
+        $('.myNavbar').addClass('shrink');
+        $('.myNavbar-nav').addClass('shrink');
+        $('.myBrand').addClass('shrink');
 
-    $('.navItemsRight').removeClass('shrink');
-    $('.navItemsLeft').removeClass('shrink');
-    $('.navLogo').removeClass('shrink');
-  }
-});
+        $('.navItemsRight').addClass('shrink');
+        $('.navItemsLeft').addClass('shrink');
+        $('.navLogo').addClass('shrink');
+
+        $scope.shrink = true;
+      } else {
+        $('.myNavbar').removeClass('shrink');
+        $('.myNavbar-nav').removeClass('shrink');
+        $('.myBrand').removeClass('shrink');
+
+        $('.navItemsRight').removeClass('shrink');
+        $('.navItemsLeft').removeClass('shrink');
+        $('.navLogo').removeClass('shrink');
+
+        $scope.shrink = false;
+      }
+    });
+  });
 });
