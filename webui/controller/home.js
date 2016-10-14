0ed732b4cd4281e08cd85e4f0d2312f836ac12f0
Locke21 <fabian.hilz@t-online.de>
2016-10-14 13:30:24 +0200

    implemented grid layout

--- webui/controller/home.js
+++ webui/controller/home.js
@@ -1,4 +1,10 @@
-noobcorner.controller('HomeCtrl',function($scope){
-  var x = $('.ContentBox')
-  $(x[0]).css('margin-top', '10px');
+noobcorner.controller('HomeCtrl',function($scope, $compile){
+  var x = $('.ContentBox')[0];
+  $(x).css('margin-top', '20px');
+  var star = $(x).children('div:first-child')
+                 .append("<i id='adminLogo' style='float: right;' class='glyphicon glyphicon-asterisk'></i>");
+  star.click(function(){
+    alert("test");
+  })
+  $compile(x)($scope);
 })
